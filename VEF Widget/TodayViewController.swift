b8a9ef43fbd3ad6230e29775dc13b8ab1fcc8395
Patrick Rengifo <patrick.rengifo@gmail.com>
2017-09-14 17:27:57 -0400

    Gather information from the APIs and show it

--- VEF Widget/TodayViewController.swift
+++ VEF Widget/TodayViewController.swift
@@ -8,12 +8,18 @@
 
 import UIKit
 import NotificationCenter
+import Alamofire
+import SwiftyJSON
 
 class TodayViewController: UIViewController, NCWidgetProviding {
         
     @IBOutlet weak var dolarTodayLabel: UILabel!
     @IBOutlet weak var localBitcoinsLabel: UILabel!
     @IBOutlet weak var usdBitcoinLabel: UILabel!
+    
+    var vefBtc : Double = 0.0
+    var usdBtc : Double = 0.0
+    var vefDtd : Double = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,8 +37,46 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         // If an error is encountered, use NCUpdateResult.Failed
         // If there's no update required, use NCUpdateResult.NoData
         // If there's an update, use NCUpdateResult.NewData
-        
-        completionHandler(NCUpdateResult.newData)
+        getData(completionHandler: completionHandler)
     }
     
+    func getData(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        Alamofire.request("https://localbitcoins.com/bitcoinaverage/ticker-all-currencies").responseJSON { response in
+            
+            if let jsonResponse = response.result.value {
+                let json = JSON(jsonResponse)
+                self.vefBtc = Double(json["VEF"]["avg_24h"].stringValue)!
+                
+                Alamofire.request("https://coinbase.com/api/v1/prices/spot_rate").responseJSON { response2 in
+                    
+                    if let jsonResponse2 = response2.result.value {
+                        let json2 = JSON(jsonResponse2)
+                        self.usdBtc = Double(json2["amount"].stringValue)!
+                        self.vefBtc = self.vefBtc / self.usdBtc
+                        self.localBitcoinsLabel.text = String(format: "1$ - Bs.%.2f", self.vefBtc)
+                        self.usdBitcoinLabel.text = String(format: "1XBT - $%.2f", self.usdBtc)
+                        
+                        Alamofire.request("http://vefexange.appjango.com/api/vefdtd").responseJSON { response3 in
+                            
+                            if let jsonResponse3 = response3.result.value {
+                                let json3 = JSON(jsonResponse3)
+                                self.vefDtd = json3["USD"]["dolartoday"].double!
+                                self.dolarTodayLabel.text = String(format: "1$ - Bs.%.2f", self.vefDtd)
+                                
+                                completionHandler(NCUpdateResult.newData)
+                            }  else {
+                                completionHandler(NCUpdateResult.failed)
+                            }
+                            
+                        }
+                    }  else {
+                        completionHandler(NCUpdateResult.failed)
+                    }
+                
+                }
+            } else {
+                completionHandler(NCUpdateResult.failed)
+            }
+        }
+    }
 }
